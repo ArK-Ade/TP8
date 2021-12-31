8b14559a2b274fd90a5c3e3e9458f9ee73f6d6d4
Alan Guenegou <Alan.Guenegou@hotmail.fr>
2021-12-31 20:38:24 +0100

    début setup thread

--- main.py
+++ main.py
@@ -5,16 +5,35 @@ import os
 import subprocess
 import re
 import threading
+import time
+import logging
 
 """
 Cette fonction simule un calcul très long
 """
 
 
-def calcul_long():
+def calcul_long(name):
+    logging.info("Thread %s: starting", name)
     n = 1E7
     while n > 0:
         n -= 1
+    logging.info("Thread %s: finishing", name)
+
+
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=calcul_long, args=(1,))
+    logging.info("Main    : before running thread")
+    x.start()
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
+
 
 
 # TODO effectuer le multi threading et multi processing sur la fonction calcul_long()
@@ -23,4 +42,3 @@ def calcul_long():
 #  Les paramètres (page 205 du pdf) seront à mettre dans un fichier texte ou mieux XML.
 #  En plus de dessiner une jolie peinture, l’objectif est de mettre en oeuvre la programmation asynchrone.
 
-print("hello")
